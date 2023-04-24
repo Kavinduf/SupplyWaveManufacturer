ad287987b58b438286f65d146162b706fb91aa6f
Kavindu Fernando <kavinduf2@gmail.com>
2023-04-24 19:45:35 +0530

    Order details

--- screens/OrderDetails.js
+++ screens/OrderDetails.js
@@ -10,12 +10,42 @@ import React from "react";
 
 const OrderDetails = () => {
   return (
-    <View>
-      <Text>OrderDetails</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.TopView}>
+        <View>
+          <Text style={styles.heading}>Order Information</Text>
+          <View style={{ flexDirection: "row", marginTop: 5 }}>
+            <Text>Order ID: </Text>
+            <Text></Text>
+          </View>
+        </View>
+      </View>
+    </SafeAreaView>
   );
 };
 
 export default OrderDetails;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight || 5,
+    backgroundColor: "#F5F5F5",
+  },
+  TopView: {
+    padding: 10,
+    backgroundColor: "#FFF",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 1,
+      height: 1,
+    },
+    shadowOpacity: 0.1,
+    shadowRadius: 2.5,
+    elevation: 1,
+  },
+  heading: {
+    fontWeight: "500",
+    fontSize: 15,
+  },
+});
